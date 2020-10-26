e7efa3682f8db38834a4d37328ff31048e30402f
rajnikantmahato <58804492+rajnikantmahato@users.noreply.github.com>
2020-10-26 19:22:04 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,21 @@
 apt install python -y
 apt install fish -y
+apt install wget -y
 cd $PREFIX/bin/
+wget https://raw.githubusercontent.com/rajnikantmahato/fun-with-python/main/rajni
+chmod +x rajni
+
+rm /data/data/com.termux/files/usr/etc/bash.bashrc
+wget https://raw.githubusercontent.com/rajnikantmahato/fun-with-python/main/bash.bashrc
+mv bash.bashrc /data/data/com.termux/files/usr/etc/
+chmod +x /data/data/com.termux/files/usr/etc/bash.bashrc
+
+
+
+wget https://raw.githubusercontent.com/rajnikantmahato/fun-with-python/main/rajni.py
+
+wget https://raw.githubusercontent.com/rajnikantmahato/fun-with-python/main/rajni2.py
+
+wget https://raw.githubusercontent.com/rajnikantmahato/fun-with-python/main/rajni3.py
+echo Thanks
+echo type rajni to run
